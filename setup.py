d9d9ea5e7735672d40e89f643617157a1a1f901a
Ken Kundert <ken@theKunderts.net>
2021-08-17 13:58:56 -0700

    enable zip_safe in installer

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ system and run commands.
 '''
 
 setup(
-    name='scripts'
-  , version='1.0.0'
-  , description='Scripting Utilities'
-  , long_description=longDescription
-  , author="Ken Kundert"
-  , author_email='ken@designers-guide.com'
-  , py_modules=['scripts']
-  , use_2to3 = True
+    name = 'scripts',
+    version = '1.0.0',
+    description = 'Scripting Utilities',
+    long_description = longDescription,
+    author = "Ken Kundert",
+    author_email = 'ken@designers-guide.com',
+    py_modules = ['scripts'],
+    zip_safe = True,
+    use_2to3 = True,
 )
